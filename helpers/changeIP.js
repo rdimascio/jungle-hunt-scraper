4933c33a1d79991ec3935decc485ea43ee6c2b79
Ryan DiMascio <ryan.dimascio@gmail.com>
2019-12-01 20:41:22 -0800

    restart tor using systemctl in prod

--- helpers/changeIP.js
+++ helpers/changeIP.js
@@ -1,11 +1,14 @@
 'use strict'
 
+require('dotenv').config()
 const exec = require('child_process').exec
 
 // Change the IP address using Tor
 const changeIpAddress = () => {
 	exec(
-		'(echo authenticate ""; echo signal newnym; echo quit) | nc localhost 9051',
+		process.env.NODE_ENV === 'development'
+			? '(echo authenticate ""; echo signal newnym; echo quit) | nc localhost 9051'
+			: 'sudo systemctl reload tor',
 		async (error, stdout, stderr) => {
 			if (stdout.match(/250/g).length === 3) {
 				console.log('IP was changed')
