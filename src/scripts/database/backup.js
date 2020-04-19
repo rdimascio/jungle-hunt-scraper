0ec43d0f33d5860ebf7b6a8fc5f47b4879d36cec
Ryan DiMascio <ryan.dimascio@gmail.com>
2020-04-19 01:25:23 -0700

    Removes path module from backup.js

--- src/scripts/database/backup.js
+++ src/scripts/database/backup.js
@@ -1,19 +1,18 @@
 require('dotenv').config()
 
 const fs = require('fs')
-const path = require('path')
 const AWS = require('aws-sdk')
 const moment = require('moment')
 
 const s3 = new AWS.S3()
-const dir = path.dirname('/var/backups/mongo/jungle-hunt/')
+const path = '/var/backups/mongo/jungle-hunt/'
 const filename = `${moment().format('MM-DD-YYYY')}.gz`
 
-if (!fs.existsSync(path.resolve(dir, filename))) {
+if (!fs.existsSync(`${path}/${filename}`)) {
     console.log(`Backup file: ${filename} does not exist.`)
 }
 
-fs.readFile(path.resolve(dir, filename), async (err, data) => {
+fs.readFile(`${path}/${filename}`, async (err, data) => {
     if (err) {
         console.log(err)
     }
