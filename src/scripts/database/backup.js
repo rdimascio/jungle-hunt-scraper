6651f25ecc4214bd381f8a4befdbb7b22a61bf0e
Ryan DiMascio <ryan.dimascio@gmail.com>
2020-04-19 00:54:37 -0700

    Changes backup filenames

--- src/scripts/database/backup.js
+++ src/scripts/database/backup.js
@@ -4,16 +4,21 @@ const fs = require('fs')
 const AWS = require('aws-sdk')
 
 const s3 = new AWS.S3()
-const path = `${new Date().toLocaleDateString().replace(/\//g, '-')}`
+const path = '/var/backups/mongo/jungle-hunt'
+const filename = `${new Date().toLocaleDateString().replace(/\//g, '-')}.gz`
 
-fs.readFile(`/var/backups/mongo/${path}/jungleHunt.gz`, async (err, data) => {
+if (!fs.existsSync(`${path}/${filename}`)) {
+    console.log(`Backup file: ${filename} does not exist.`)
+}
+
+fs.readFile(`${path}/${filename}`, async (err, data) => {
     if (err) {
         console.log(err)
     }
 
     const s3params = {
         Bucket: `jungle-hunt/backups/`,
-        Key: `${path}.gz`,
+        Key: `${filename}`,
         Body: data,
         ContentEncoding: 'gzip'
     }
